ef45c3af5ffddf6aa810eedfbf72ac4dcc3d8cad
alina <aliina.tudose@gmail.com>
2016-07-27 15:58:55 +0200

    update user project

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -18,6 +18,22 @@ class ProjectsController < ApplicationController
       # render 'error'
     end
   end
+  def edit
+    @user = User.find_by_id(params[:user_id])
+    @project = Project.find_by_id(params[:id])
+  end
+  def update
+    @user = User.find_by_id(params[:user_id])
+    @project = Project.find_by_id(params[:id])
+    if @project.update_attributes(name:params[:project][:name], description:params[:project][:description], avatar:params[:project][:avatar], user_id:params[:user_id], tag_list:params[:project][:tag_list] )
+      redirect_to user_project_path(@user.id, @project.id)
+    else
+      render 'edit'
+    end
+  end
+  def show
+    @project = Project.find_by_id(params[:id])
+  end
 
   # private
 
