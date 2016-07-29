b32f512303cefdae1b34c9069fe2ae823437665d
alina <aliina.tudose@gmail.com>
2016-07-29 20:12:36 +0200

    fix javascript errors

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,6 +13,7 @@
 
 //= require jquery
 //= require jquery_ujs
+//= require jquery.touchSwipe
 
 //= require jquery.turbolinks
 //= require chosen-jquery
@@ -24,18 +25,60 @@
 
 //= require_tree .
 
-$(function() {
-    //Enable swiping...
-    $(".js-extended").swipe( {
-      //Generic swipe handler for all directions
-      swipe:function(event, direction, distance, duration, fingerCount, fingerData) {
-        // function goToURL() {
-        //   location.href = 'http://google.it';
-        //
-        // }
-        $(this).text("You swiped " + direction );
-      },
-      //Default is 75px, set to 0 for demo so any distance triggers swipe
-       threshold:0
-    });
+
+$(document).ready(function(){
+  //Enable swiping...
+  $(".js-extended").swipe( {
+    //Generic swipe handler for all directions
+    swipe:function(event, direction, distance, duration, fingerCount, fingerData) {
+      // function goToURL() {
+      //   location.href = 'http://google.it';
+      //
+      // }
+      $(this).text("You swiped " + direction );
+    },
+    //Default is 75px, set to 0 for demo so any distance triggers swipe
+     threshold:0
   });
+
+// Show signup form in two steps
+  $('.signup-speaker-btn').click(function() {
+     if($('#speaker-btn').is(':checked')) {
+       $('.second-step').removeClass('hidden')
+       $('.first-step').addClass('hidden')
+      }
+  });
+  $('.signup-attendee-btn').click(function() {
+     if($('#attendee-btn').is(':checked')) {
+       $('.second-step').removeClass('hidden')
+       $('.first-step').addClass('hidden')
+      }
+  });
+
+// style material form
+  var materialForm;
+  materialForm = function () {
+      return $('input').focus(function () {
+          return $(this).closest('.field').addClass('focused has-value');
+      }).focusout(function () {
+          return $(this).closest('.field').removeClass('focused');
+      }).blur(function () {
+          if (!this.value) {
+              $(this).closest('.field').removeClass('has-value');
+          }
+          return $(this).closest('.field').removeClass('focused');
+      });
+  };
+  $(function () {
+      return materialForm();
+  });
+
+  //call side nav
+  $(".button-collapse").sideNav();
+
+  //swipe alerts away
+  var $alert = $('.alert');
+  if($alert.length > 0){
+    Materialize.toast($alert, 5000);
+  }
+});
