afb7cf031f7f761c3022dcee6742774356059afe
alina <aliina.tudose@gmail.com>
2016-07-30 17:48:26 +0200

    customize activeadmin dashboard and resources

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -4,30 +4,34 @@ ActiveAdmin.register_page "Dashboard" do
 
   content title: proc{ I18n.t("active_admin.dashboard") } do
     div class: "blank_slate_container", id: "dashboard_default_message" do
-      span class: "blank_slate" do
-        span I18n.t("active_admin.dashboard_welcome.welcome")
-        small I18n.t("active_admin.dashboard_welcome.call_to_action")
-      end
+      # span class: "blank_slate" do
+      #   span I18n.t("active_admin.dashboard_welcome.welcome")
+      #   small I18n.t("active_admin.dashboard_welcome.call_to_action")
+      # end
     end
 
     # Here is an example of a simple dashboard with columns and panels.
-    #
-    # columns do
-    #   column do
-    #     panel "Recent Posts" do
-    #       ul do
-    #         Post.recent(5).map do |post|
-    #           li link_to(post.title, admin_post_path(post))
-    #         end
-    #       end
-    #     end
-    #   end
 
-    #   column do
-    #     panel "Info" do
-    #       para "Welcome to ActiveAdmin."
-    #     end
-    #   end
-    # end
-  end # content
+    columns do
+      column do
+        panel "Recent Users" do
+          ul do
+            table_for User.order("created_at desc").limit(5) do
+              column :name do |user|
+                link_to user.name, admin_panel_user_path(user)
+              end
+              column :created_at
+            end
+            strong { link_to "View All Users", admin_panel_users_path }
+          end
+        end
+      end
+
+      column do
+        panel "Info" do
+          para "Welcome to ActiveAdmin."
+        end
+      end
+    end
+  end
 end
--- app/admin/speaker.rb
+++ app/admin/speaker.rb
@@ -1,16 +1,16 @@
-ActiveAdmin.register User do
-  # index do
-  #   column :name
-  #   column :email
-  #   column :summary
-  #   column :twitter
-  #   column :linkedin
-  #   column :github
-  # end
+ActiveAdmin.register Speaker do
+  index do
+    column :name
+    column :email
+    column :twitter
+    column :linkedin
+    column :github
+    actions
+  end
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
-permit_params :list, :of, :attributes, :on, :model
+# permit_params :list, :of, :attributes, :on, :model
 #
 # or
 #
